ce2625f73b9354add77aec4c4e9619cbfaf0a240
Mykola Kushnir <mkushnir885@gmail.com>
2025-12-15 22:57:13 +0200

    chore: set up default Express route

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/', (_, res) => {
+  res.sendFile('menu.html', { root: 'public' });
+});
+
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
 });
